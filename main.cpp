87382177ea0e27cf773957f29fc6f3411138b433
Alexey <a.astafiev2016@gmail.com>
2022-08-02 22:01:36 +0300

    code:тесты

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab03-test
+//
+//  Created by Alexey Astafiev on 02.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  lab03-test
+//
+//  Created by Alexey Astafiev on 02.08.2022.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
